1e16614f2ec37be57784d55e15240920bee40122
wangtianyang <wangtianyang@hyjf.com>
2019-01-11 15:57:35 +0800

    添加功能

--- src/pages/accout/generator.js
+++ src/pages/accout/generator.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import * as mock from './../../mock';
 import ClipboardJS from 'clipboard';
-import { Icon, Button,Form, Input, message, Switch} from 'antd';
+import { Icon, Button,Form, Input, message, Select, Col  } from 'antd';
 const FormItem = Form.Item;
+const Option = Select.Option;
 class SelectInput extends Component{
   render(){
     return (<Input className="select-input" 
@@ -26,8 +27,26 @@ class AccountGenerator extends Component {
     rendomID:'',
     rendomMobile: '',
     rendomBankCard: '',
+    cardBin:'622848',
     defaultBankCard: '6228481234567890003',
-    adult: true
+    adult: true,
+    cardBinList : [
+      { name: "农业银行", value: '622848' },
+      { name: "招商银行", value: '622580' },
+      { name: "民生银行", value: '622622' },
+      { name: "建设银行", value: '622728' },
+      { name: "中国银行", value: '622763' },
+      { name: "兴业银行", value: '622909' },
+      { name: "工商银行", value: '623062' },
+      { name: "浦发银行", value: '622228' },
+      { name: "中信银行", value: '621771' },
+      { name: "光大银行", value: '622663' },
+      { name: "民生银行", value: '622617' },
+      { name: "交通银行", value: '622258' },
+      { name: "平安银行", value: '622156' },
+      { name: "邮政银行", value: '620062' },
+      { name: "华夏银行", value: '621222' }
+    ]
   };
   componentDidMount(){
     this.initRendomData()
@@ -41,24 +60,6 @@ class AccountGenerator extends Component {
     });
   }
 
-  rendomNameData = (rule, value, callback) => {
-    const form = this.props.form;
-    if (value && value !== form.getFieldValue('password')) {
-      callback('Two passwords that you enter is inconsistent!');
-    } else {
-      callback();
-    }
-  }
-
-  getFullAccountData = ()=>{
-    //   const dir = {
-    //     rendomName: '姓名',
-    //     rendomID:'身份证',
-    //     rendomMobile: '手机',
-    //     rendomBankCard: '银行卡'
-    //   }
-      return this.state
-  }
 
   /**
    * 伸展菜单
@@ -74,9 +75,19 @@ class AccountGenerator extends Component {
   * 更改生成信息成年人/未成年
   * 
   */
-  changeAdult= ()=>{
-    this.setState({
+  changeAdult = async ()=>{
+    await this.setState({
       adult: !this.state.adult
+    })
+    this.initRendomData()
+  }
+  /**
+  * 更改卡bin
+  * 
+  */
+  changeCardBin = (cardBin = this.state.cardBin)=>{
+    this.setState({
+      cardBin:cardBin
     })
     this.initRendomData();
   }
@@ -92,12 +103,10 @@ class AccountGenerator extends Component {
       rendomName: mock.rendomName(),
       rendomID: mock.rendomID(this.state.adult),
       rendomMobile: mock.rendomMobile(),
-      rendomBankCard: mock.rendomBankCard()
+      rendomBankCard: mock.rendomBankCard(this.state.cardBin)
     })
   }
   render() {
-      this.getFullAccountData()
-    const { getFieldDecorator } = this.props.form;
     const formItemLayout = {
       labelCol: {
         xs: { span: 24 },
@@ -123,35 +132,45 @@ class AccountGenerator extends Component {
     return (
       <div className="App">
         <FormItem inline  {...tailFormItemLayout}>
-          <label className="ant-checkbox-inline" style={{marginRight:10}} title="切换会重新生成数据">成年人/未成年  <Switch defaultChecked={this.state.adult} onChange={this.changeAdult} /></label>
+          
           <Button type="primary" htmlType="button"  onClick={this.initRendomData}>重新生成</Button>
           <Button type="primary" htmlType="button" style={{ marginLeft: 8 }} data-clipboard-text={Object.values(this.state).join(',')}>复制全部</Button>
         </FormItem>
         <Form>
           <FormItem {...formItemLayout} label="姓名">
-              {getFieldDecorator('rendomName')(
-                  <SelectInput prefix="user" rendomValue={this.state.rendomName} ></SelectInput>
-              )}
+            <SelectInput prefix="user" rendomValue={this.state.rendomName} ></SelectInput>
           </FormItem>
           <FormItem {...formItemLayout} label="身份证号">
-              {getFieldDecorator('rendomID')(
-                  <SelectInput prefix="idcard" rendomValue={this.state.rendomID}></SelectInput>
-              )}
+            <Col span="6">
+              <Select defaultValue={1} onChange={this.changeAdult} style={{marginRight:10}}>
+                <Option value={1}>成年人</Option>
+                <Option value={0}>未成年</Option>
+              </Select>
+            </Col>
+            <Col span="18"><SelectInput prefix="idcard" rendomValue={this.state.rendomID}></SelectInput></Col>
+            
           </FormItem>
           <FormItem {...formItemLayout} label="手机号">
-              {getFieldDecorator('rendomMobile')(
-                  <SelectInput prefix="mobile" rendomValue={this.state.rendomMobile}></SelectInput>
-              )}
+            <SelectInput prefix="mobile" rendomValue={this.state.rendomMobile}></SelectInput>
           </FormItem>
           <FormItem {...formItemLayout} label="固定银行卡号">
-              {getFieldDecorator('defaultBankCard')(
-                  <SelectInput prefix="credit-card" rendomValue={this.state.defaultBankCard}></SelectInput>
-              )}
+            <SelectInput prefix="credit-card" rendomValue={this.state.defaultBankCard}></SelectInput>
           </FormItem>
           <FormItem {...formItemLayout} label="银行卡号">
-              {getFieldDecorator('rendomBankCard')(
-                  <SelectInput prefix="credit-card" rendomValue={this.state.rendomBankCard}></SelectInput>
-              )}
+            <Col span="6">
+              <Select defaultValue={this.state.cardBin} onChange={this.changeCardBin}>
+                {
+                  this.state.cardBinList.map((element) => {
+                    return (<Option value={element.value} key={element.value}>{element.name}</Option>)
+                  })
+                }
+              </Select>
+            </Col>
+            <Col span="18">
+              <SelectInput prefix="credit-card" rendomValue={this.state.rendomBankCard}></SelectInput>
+              
+            </Col>
+            
           </FormItem>
         </Form>
       </div>
